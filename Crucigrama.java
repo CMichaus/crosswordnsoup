8cfac7cb33d09446bcf3c6f44c742e165b5e74bc
Otniel Aguilar <otnieel.aguilar@gmail.com>
2013-11-20 15:51:46 -0600

    Se agregan metodos que pinten palabras, faltan validaciones

--- Crucigrama.java
+++ Crucigrama.java
@@ -4,11 +4,22 @@ import javax.swing.*;
  
 public class Crucigrama extends JFrame {
     private LoadImage crucigrama;
-    private JLabel score, tiempoRestante;
+    private JLabel lblpregunta, score, tiempoRestante;
     private JTextArea pregunta, respuesta;
     private JButton comprobarRespuesta, salir;
-    private String preguntas[] = {"Pregunta 1", "Pregunta 2", "Pregunta 3", "Pregunta 4"};
-    private JComboBox listaPreguntas;
+    private String numero_preguntas[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+    private String preguntas[] = {"Es un algoritmo de búsqueda de raíces que trabaja dividiendo el intervalo a la mitad \ny seleccionando el subintervalo que tiene la raíz.", 
+				  "El procedimiento empieza con una estimación o conjetura inicial \nde x, que es mejorada por iteración hasta alcanzar la convergencia. \nPara que converja, la derivada (dg/dx) debe ser menor que 1 \nen magnitud (al menos para los valores x que se encuentran \ndurante las iteraciones). La convergencia será establecida \nmediante el requisito de que el cambio en x de una iteración a \nla siguiente no sea mayor en magnitud que \nalguna pequeña cantidad ε.", 
+				  "Es una variación del método de Newton-Raphson donde en vez de calcular la derivada \nde la función en el punto de estudio, teniendo en mente la definición de derivada, se \naproxima la pendiente a la recta que une la función evaluada en el punto de estudio \ny en el punto de la iteración anterior.", 
+				  "Como en el método de bisección, se parte de un intervalo inicial [a0,b0] con f(a0) \ny f(b0) de signos opuestos, lo que garantiza que en su interior hay al menos una \nraíz. El algoritmo va obteniendo sucesivamente en cada paso un intervalo más \npequeño [ak, bk] que sigue incluyendo una raíz de la función f.", 
+				  "Es un método abierto, en el sentido de que su convergencia global no está \ngarantizada. La única manera de alcanzar la convergencia es seleccionar \nun valor inicial lo suficientemente cercano a la raíz buscada.", 
+				  "El polinomio de interpolación de Newton de forma _____ ________ se puede determinar \nasumiendo la siguiente forma: \nPn(x) = c0 + c1 (x − x0 ) + c2 (x − x0 )(x − x1 ) + · · · + cn (x − x0 ) · · · (x − xn−1 ) \ndonde los coeficientes ck , k = 0, . . . , n se determinan al cumplir con las \nrestricciones Pn (xi ) = yi , i = 0, . . . , n.", 
+				  "Es un método iterativo, usado para resolver sistemas de ecuaciones lineales \ndel tipo Ax=b. El algoritmo toma su nombre del matemático alemán Carl \nGustav Jakob Jacobi. Consiste en usar fórmulas como iteración de punto fijo.", 
+				  "El método consiste en ir -pivoteando- en la diagonal principal. Se comienza en \nel extremo superior izquierdo, el renglón donde esta el pivote va a ser el \nrenglón base de todo el sistema y la columna donde esta el pivote va a ser \nla columna base. Con respecto a ese renglón y esa columna, donde está el \npivote, se forman determinantes de dos por dos, y siempre se trabaja con \nnúmeros enteros, si apareciera alguna fracción hay un error.", 
+				  "Un sistema de ecuaciones se resuelve por este método cuando se obtienen sus \nsoluciones mediante la reducción del sistema dado a otro equivalente en el que \ncada ecuación tiene una incógnita menos que la anterior. Este método transforma \nla matriz de coeficientes en una matriz triangular superior. Después, este método \ncontinúa el proceso de transformación hasta obtener una matriz diagonal.", 
+				  "Este método para resolver este tipo de sistemas consiste, por tanto, en representar \nen unos ejes cartesianos, o sistema de coordenadas, ambas rectas y comprobar si \nse cortan y, si es así, dónde."};    
+
+    private JComboBox<String> listaPreguntas;
 
     public Crucigrama() {
 	Container cp = this.getContentPane();
@@ -19,11 +30,22 @@ public class Crucigrama extends JFrame {
 	constraints.gridy = 0;
 	constraints.gridwidth = 1;
 	constraints.gridheight = 1;
-	cp.add(new JLabel("Selecciona la Pregunta"), constraints);
+	lblpregunta = new JLabel("Selecciona la Pregunta");
+	lblpregunta.setFont(new Font("Serif", Font.PLAIN, 25));
+	cp.add(lblpregunta, constraints);
 	
 	
-	listaPreguntas = new JComboBox(preguntas);
-	listaPreguntas.setSelectedIndex(1);
+	listaPreguntas = new JComboBox<String>(numero_preguntas);
+	listaPreguntas.setSelectedIndex(0);
+
+	listaPreguntas.addActionListener(new ActionListener() {
+		@Override
+		    public void actionPerformed(ActionEvent e) {
+		    JComboBox<Object> cb = (JComboBox<Object>)e.getSource();
+		    int pregunta = cb.getSelectedIndex();
+		    updateQuestion(pregunta);
+		}
+	    });
 	constraints.gridx = 0;
 	constraints.gridy = 1;
 	constraints.gridwidth = 1;
@@ -40,7 +62,8 @@ public class Crucigrama extends JFrame {
         constraints.gridy = 3;
         constraints.gridwidth = 1;
         constraints.gridheight = 1;
-	pregunta = new JTextArea("Metodo que...", 5, 30);
+	pregunta = new JTextArea(preguntas[0], 5, 30);
+	pregunta.setEditable(false);
 	cp.add(pregunta, constraints);
 	
 	constraints.gridx = 0;
@@ -53,7 +76,7 @@ public class Crucigrama extends JFrame {
         constraints.gridy = 5;
         constraints.gridwidth = 1;
         constraints.gridheight = 1;
-	respuesta = new JTextArea("Escribe la respuesta (:", 5, 30);
+	respuesta = new JTextArea("Respuesta", 5, 30);
 	cp.add(respuesta, constraints);
 	
        	tiempoRestante = new JLabel("Tiempo Restante");
@@ -63,7 +86,7 @@ public class Crucigrama extends JFrame {
         constraints.gridheight = 1;
 	cp.add(tiempoRestante, constraints);
 	
-	crucigrama = new LoadImage("imagenes/crucigrama.jpg");
+	crucigrama = new LoadImage("imagenes/primero.png");
 	constraints.gridx = 1;
         constraints.gridy = 1;
         constraints.gridwidth = 3;
@@ -71,6 +94,13 @@ public class Crucigrama extends JFrame {
 	cp.add(crucigrama, constraints);
 	
 	comprobarRespuesta = new JButton("Comprobar Respuesta");
+	comprobarRespuesta.addActionListener(new ActionListener() {
+		@Override
+		    public void actionPerformed(ActionEvent e) {
+		    
+		}
+	    });
+
 	constraints.gridx = 1;
         constraints.gridy = 5;
         constraints.gridwidth = 1;
@@ -94,13 +124,19 @@ public class Crucigrama extends JFrame {
 	
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        	setTitle("Crucigrama");
-	setSize(800, 600);  
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	setSize(screenSize);  
 	setLocationRelativeTo(null);
-	pack();
 	setVisible(true);    // show it
 	
     }
  
+    
+    protected void updateQuestion(int preg) {
+        pregunta.setText(preguntas[preg]);
+    }
+
+
     /** The entry main() method */
     public static void main(String[] args) {
 	// Run GUI codes in Event-Dispatching thread for thread-safety
--- LoadImage.java
+++ LoadImage.java
@@ -14,7 +14,156 @@ public class LoadImage extends Component {
 
     public void paint(Graphics g) {
         g.drawImage(img, 0, 0, null);
+	paintBiseccion(g);
+	paintFalsaPosicion(g);
+	//paintMontante(g);
+	paintJacobi(g);
+	paintGaussJordan(g);
+	//paintSecante(g);
+	paintNewtonRaphson(g);
+	//paintHaciaAdelante(g);
+	paintGrafico(g);
+	paintPuntoFijo(g);
     }
+
+
+    public void paintBiseccion(Graphics g) {
+	g.setFont(new Font("Serif", Font.BOLD, 20));
+	g.drawString("B", 445, 35);
+	g.drawString("I", 450, 69);
+	g.drawString("S", 448, 105);
+	g.drawString("E", 447, 142);
+	g.drawString("C", 447, 177);
+	g.drawString("C", 447, 213);
+	g.drawString("I", 450, 247);
+	g.drawString("O", 447, 285);
+	g.drawString("N", 445, 320);
+    }
+    
+    public void paintGaussJordan(Graphics g) {
+	g.setFont(new Font("Serif", Font.BOLD, 20));
+	g.drawString("G", 339, 106);
+	g.drawString("A", 375, 105);
+	g.drawString("U", 409, 105);
+	g.drawString("S", 448, 105);
+	g.drawString("S", 481, 105);
+	g.drawString("J", 521, 105);
+	g.drawString("O", 552, 105);
+	g.drawString("R", 587, 105);
+	g.drawString("D", 622, 105);
+	g.drawString("A", 657, 105);
+	g.drawString("N", 690, 105);	
+    }
+    
+    public void paintMontante(Graphics g) {
+	g.setFont(new Font("Serif", Font.BOLD, 20));
+	g.drawString("M", 550, 70);
+	g.drawString("O", 552, 105);
+	g.drawString("N", 550, 141);
+	g.drawString("T", 550, 178);
+	g.drawString("A", 550, 214);
+	g.drawString("N", 550, 247);
+	g.drawString("T", 550, 285);
+	g.drawString("E", 552, 320);	
+    }
+
+    public void paintPuntoFijo(Graphics g) {
+	g.drawString("P", 88, 285);
+	g.drawString("U", 128, 285);
+	g.drawString("N", 160, 285);
+	g.drawString("T", 196, 285);
+	g.drawString("O", 232, 285);
+	g.drawString("F", 265, 285);
+	g.drawString("I", 300, 285);
+	g.drawString("J", 339, 285);
+	g.drawString("O", 375, 285);
+    }
+
+    public void paintSecante(Graphics g) { 
+	g.setFont(new Font("Serif", Font.BOLD, 20));
+	g.drawString("S", 520, 320);	
+	g.drawString("E", 552, 320);	
+	g.drawString("C", 587, 320);
+	g.drawString("A", 622, 320);
+	g.drawString("N", 657, 320);
+	g.drawString("T", 695, 320);
+	g.drawString("E", 730, 320);
+    }
+
+    public void paintHaciaAdelante(Graphics g) {
+	g.setFont(new Font("Serif", Font.BOLD, 20));
+	g.drawString("H", 18, 498);
+	g.drawString("A", 52, 498);
+	g.drawString("C", 88, 498);
+	g.drawString("I", 128, 498);
+	g.drawString("A", 160, 498);
+	g.drawString("A", 196, 498);
+	g.drawString("D", 232, 498);
+	g.drawString("E", 265, 498);
+	g.drawString("L", 300, 498);
+	g.drawString("A", 339, 498);
+	g.drawString("N", 375, 498);
+	g.drawString("T", 410, 498);
+	g.drawString("E", 446, 498);
+    }
+
+    public void paintNewtonRaphson(Graphics g) {
+	g.setFont(new Font("Serif", Font.BOLD, 20));
+	g.drawString("N", 18, 178);
+	g.drawString("E", 18, 214);
+	g.drawString("W", 18, 247);
+	g.drawString("T", 18, 285);
+	g.drawString("O", 18, 320);	
+	g.drawString("N", 18, 353);
+	g.drawString("R", 18, 390);
+	g.drawString("A", 18, 428);
+	g.drawString("P", 19, 462);
+	g.drawString("H", 18, 498);
+	g.drawString("S", 19, 533);
+	g.drawString("O", 18, 571);
+	g.drawString("N", 18, 604);
+    }
+    
+    public void paintJacobi(Graphics g) {
+	g.setFont(new Font("Serif", Font.BOLD, 20));
+	g.drawString("J", 660, 70);
+	g.drawString("A", 657, 105);
+	g.drawString("C", 656, 141);
+	g.drawString("O", 656, 178);
+	g.drawString("B", 656, 214);
+	g.drawString("I", 659, 247);
+
+    }
+    
+    public void paintGrafico(Graphics g) { 
+	g.setFont(new Font("Serif", Font.BOLD, 20));
+	g.drawString("G", 232, 428);
+	g.drawString("R", 265, 428);
+	g.drawString("A", 300, 428);
+	g.drawString("F", 339, 428);
+	g.drawString("I", 377, 428);
+	g.drawString("C", 410, 428);
+	g.drawString("O", 446, 428);
+    }
+
+    public void paintFalsaPosicion(Graphics g) {
+	g.setFont(new Font("Serif", Font.BOLD, 20));
+	g.drawString("F", 375, 70);
+	g.drawString("A", 375, 105);
+	g.drawString("L", 375, 141);
+	g.drawString("S", 375, 178);
+	g.drawString("A", 375, 214);
+	g.drawString("P", 375, 247);
+	g.drawString("O", 375, 285);
+	g.drawString("S", 375, 320);
+	g.drawString("I", 377, 353);
+	g.drawString("C", 375, 390);
+	g.drawString("I", 377, 428);
+	g.drawString("O", 375, 462);
+	g.drawString("N", 375, 498);
+    }
+    
+
 
     public LoadImage(String nombre) {
        try {
